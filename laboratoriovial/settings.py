c12dee6816a5fc0f3627e391d62fb7335a1bc7ae
remuzzu <remuzzu@gmail.com>
2022-09-19 12:01:39 -0300

    secret-key

--- laboratoriovial/settings.py
+++ laboratoriovial/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+from decouple import config
+secret_key = config('SECRET_KEY')
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#(&1b7b+)tq$=tmloijh1ta-lc5_2@=k)*lpqa*5=4!tq#bz!q'
+SECRET_KEY = secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
