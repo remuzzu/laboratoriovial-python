24e5a3b56b91a7b733379e4cb8d33d3635af2f18
remuzzu <remuzzu@gmail.com>
2022-09-26 11:21:52 -0300

    admin

--- apps/publicaciones/admin.py
+++ apps/publicaciones/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from apps.publicaciones.models import Congreso, Publicacion
 
 # Register your models here.
+admin.site.register(Congreso)
+
+admin.site.register(Publicacion)
