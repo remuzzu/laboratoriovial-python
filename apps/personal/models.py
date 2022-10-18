74d8f26129856eee40f613f535a4007db604b172
remuzzu <remuzzu@gmail.com>
2022-10-18 13:15:37 -0300

    improve-personal

--- apps/personal/models.py
+++ apps/personal/models.py
@@ -1,3 +1,4 @@
+from distutils.command.upload import upload
 from django.db import models
 from django import forms
 
@@ -6,9 +7,12 @@ from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 class Persona(models.Model):
-   nombre = models.CharField(max_length=50)
-   descripcion = models.CharField(max_length=200, blank=True)
-   imagen = models.CharField(max_length=200, blank=True)
+   id = models.AutoField(primary_key=True)
+   nombre = models.CharField(max_length=50, verbose_name='Nombre')
+   #descripcion = models.CharField(max_length=200, blank=True)
+   # verbose_name informa al usuario como se llama ese campo
+   descripcion = models.TextField(null=True, verbose_name='Descripción')
+   imagen = models.ImageField(upload_to = 'imagenes/', null=True, verbose_name='Imágen')
    
    def __str__(self) -> str:
       return '{} - {}'.format(self.nombre, self.descripcion)
